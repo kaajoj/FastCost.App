5efe7e197d5cd41c245e2167fc15474bb3048d0b
Karol <kaajooj@gmail.com>
2023-03-07 01:37:17 +0100

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FastCost;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FastCost;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
