843e0da81e604d4d8f21e79f5b70f6b371d7656f
Karol <kaajooj@gmail.com>
2023-03-08 02:56:31 +0100

    Add OnCostClicked and update CostBtn

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,9 +2,20 @@
 
 public partial class MainPage : ContentPage
 {
+	int allCosts = 0;
+
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	private void OnCostClicked(object sender, EventArgs e)
+	{
+        allCosts++;
+
+        CostBtn.Text = $"Cost {allCosts} added";
+
+        SemanticScreenReader.Announce(CostBtn.Text);
 	}
 }
 
