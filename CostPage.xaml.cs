8e4d22ad6c3b8f64d5609da51daf100261c679c8
Karol <kaajooj@gmail.com>
2023-03-15 04:13:43 +0100

    Add CostPage and save text to file

---
+++ CostPage.xaml.cs
@@ -0,0 +1,26 @@
+namespace FastCost;
+
+public partial class CostPage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "costs.txt");
+    public CostPage()
+	{
+		InitializeComponent();
+
+        if (File.Exists(_fileName))
+            TextEditor.Text = File.ReadAllText(_fileName);
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        TextEditor.Text = string.Empty;
+    }
+}
