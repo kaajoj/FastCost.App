a4acd21a8f06dc52580e572d408c962d6ea56a6e
Karol <kaajooj@gmail.com>
2023-03-22 00:36:22 +0100

    Update AllCostsPage

--- Views/AllCostsPage.xaml.cs
+++ Views/AllCostsPage.xaml.cs
@@ -5,5 +5,32 @@ public partial class AllCostsPage : ContentPage
 	public AllCostsPage()
 	{
 		InitializeComponent();
-	}
+
+        BindingContext = new Models.AllCosts();
+    }
+
+    protected override void OnAppearing()
+    {
+        ((Models.AllCosts)BindingContext).LoadNotes();
+    }
+
+    private async void Add_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(CostPage));
+    }
+
+    private async void costsCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.Count != 0)
+        {
+            // Get the cost model
+            var note = (Models.Cost)e.CurrentSelection[0];
+
+            // Should navigate to "CostPage?ItemId=path\on\device\XYZ.costs.txt"
+            await Shell.Current.GoToAsync($"{nameof(CostPage)}?{nameof(CostPage.ItemId)}={cost.FileName}");
+
+            // Unselect the UI
+            costsCollection.SelectedItem = null;
+        }
+    }
 }
