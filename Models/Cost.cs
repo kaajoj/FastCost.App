4f49c1f233c462fab156b0e0950d289a38069907
Karol <kaajooj@gmail.com>
2023-03-21 01:58:20 +0100

    Updated CostPage

---
+++ Models/Cost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastCost.Models
+{
+    public class Cost
+    {
+        public string FileName { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        // test
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    }
+}
