dcc3272ff761e2c12429dc1ccc4b8887b12c1e4f
Karol <kaajooj@gmail.com>
2023-04-16 22:19:33 +0200

    Add CostsDatabase.cs

---
+++ DAL/CostsDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastCost.Models;
+using Microsoft.VisualBasic;
+using SQLite;
+
+namespace FastCost.DAL
+{
+    public class CostsDatabase
+    {
+        SQLiteAsyncConnection Database;
+
+        public CostsDatabase()
+        {
+        }
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(ConfigDb.DatabasePath, ConfigDb.Flags);
+            var result = await Database.CreateTableAsync<Cost>();
+        }
+        
+    }
+}
