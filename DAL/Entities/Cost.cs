d30810ca22ec1bbe8d824e7c64ad77430a2b62e6
Karol <kaajooj@gmail.com>
2023-04-16 22:23:28 +0200

    Add Cost entity

---
+++ DAL/Entities/Cost.cs
@@ -0,0 +1,10 @@
+﻿namespace FastCost.DAL.Entities
+{
+    public class Cost
+    {
+        public decimal Value { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string FileName { get; set; }
+    }
+}
