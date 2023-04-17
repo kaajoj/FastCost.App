8e2804b205c7d100e7fdcf6f21d0cd95d96567c2
Karol <kaajooj@gmail.com>
2023-04-18 01:47:29 +0200

    Register CostPage and CostRepository

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using FastCost.DAL;
+using FastCost.Views;
+using Microsoft.Extensions.Logging;
 
 namespace FastCost;
 
@@ -15,8 +17,11 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
+        builder.Services.AddSingleton<CostRepository>();
+        builder.Services.AddTransient<CostPage>();
+
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
