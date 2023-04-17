c5b81c83d927a589b33491d96233a05ac4e23e0a
Karol <kaajooj@gmail.com>
2023-04-18 00:56:45 +0200

    Add crud methods

--- DAL/CostsDatabase.cs
+++ DAL/CostsDatabase.cs
@@ -24,6 +24,41 @@ namespace FastCost.DAL
             Database = new SQLiteAsyncConnection(ConfigDb.DatabasePath, ConfigDb.Flags);
             var result = await Database.CreateTableAsync<Cost>();
         }
-        
+
+        public async Task<List<Cost>> GetCostsAsync()
+        {
+            await Init();
+            return await Database.Table<Cost>().ToListAsync();
+        }
+
+        // public async Task<List<Cost>> GetCostsNotDoneAsync()
+        // {
+        //     await Init();
+        //     return await Database.Table<Cost>().Where(t => t.Done).ToListAsync();
+        //
+        //     // SQL queries are also possible
+        //     //return await Database.QueryAsync<TodoCost>("SELECT * FROM [TodoCost] WHERE [Done] = 0");
+        // }
+
+        public async Task<Cost> GetCostAsync(int id)
+        {
+            await Init();
+            return await Database.Table<Cost>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<int> SaveCostAsync(Cost cost)
+        {
+            await Init();
+            if (cost.Id != 0)
+                return await Database.UpdateAsync(cost);
+            else
+                return await Database.InsertAsync(cost);
+        }
+
+        public async Task<int> DeleteCostAsync(Cost cost)
+        {
+            await Init();
+            return await Database.DeleteAsync(cost);
+        }
     }
 }
