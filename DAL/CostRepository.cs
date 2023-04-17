b2773bcf24437b8836c6d9f18f117e12a29c9b80
Karol <kaajooj@gmail.com>
2023-04-18 01:45:27 +0200

    Rename to CostRepository v2

--- DAL/CostRepository.cs
+++ DAL/CostRepository.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FastCost.DAL.Entities;
+using FastCost.Models;
 using SQLite;
 
 namespace FastCost.DAL
 {
-    public class CostsDatabase
+    public class CostRepository
     {
         SQLiteAsyncConnection Database;
 
-        public CostsDatabase()
+        public CostRepository()
         {
         }
 
