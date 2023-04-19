e082930aa2440036270730644e0e56f4a7c18f58
Karol <kaajooj@gmail.com>
2023-04-19 02:20:14 +0200

    Use GetCostsAsync from costRepo in AllCosts

--- Models/AllCosts.cs
+++ Models/AllCosts.cs
@@ -19,27 +19,27 @@ namespace FastCost.Models
         {
             Costs.Clear();
 
-            var result = await _costRepository.GetCostsAsync();
+            var costs = await _costRepository.GetCostsAsync();
 
             // Get the folder where the costs are stored.
-            string appDataPath = FileSystem.AppDataDirectory;
+            // string appDataPath = FileSystem.AppDataDirectory;
 
-            // Use Linq extensions to load the *.costs.txt files.
-            IEnumerable<Cost> costs = Directory
-                // Select the file names from the directory
-                .EnumerateFiles(appDataPath, "*.costs.txt")
-                // Each file name is used to create a new Cost
-                .Select(fileName => new Cost()
-                {
-                    FileName = fileName,
-                    Description = File.ReadAllText(fileName),
-                    Date = File.GetCreationTime(fileName)
-                })
-                // With the final collection of costs, order them by date
-                .OrderBy(cost => cost.Date);
+            // // Use Linq extensions to load the *.costs.txt files.
+            // IEnumerable<Cost> costs = Directory
+            //     // Select the file names from the directory
+            //     .EnumerateFiles(appDataPath, "*.costs.txt")
+            //     // Each file name is used to create a new Cost
+            //     .Select(fileName => new Cost()
+            //     {
+            //         FileName = fileName,
+            //         Description = File.ReadAllText(fileName),
+            //         Date = File.GetCreationTime(fileName)
+            //     })
+            //     // With the final collection of costs, order them by date
+            //     .OrderBy(cost => cost.Date);
 
             // Add each cost into the ObservableCollection
-            foreach (Cost cost in costs)
+            foreach (Cost cost in costs.OrderBy(cost => cost.Date))
                 Costs.Add(cost);
         }
     }
