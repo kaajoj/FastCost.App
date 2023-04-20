21ea0e126a108cc4d0a853baa118757e0f63db92
Karol <kaajooj@gmail.com>
2023-04-21 01:08:58 +0200

    clean up AllCosts

--- Models/AllCosts.cs
+++ Models/AllCosts.cs
@@ -9,7 +9,6 @@ namespace FastCost.Models
 
         public AllCosts()
         {
-
         }
 
         public async Task LoadCosts()
@@ -18,25 +17,7 @@ namespace FastCost.Models
 
             var costs = await App.CostRepository.GetCostsAsync();
 
-            // Get the folder where the costs are stored.
-            // string appDataPath = FileSystem.AppDataDirectory;
-
-            // // Use Linq extensions to load the *.costs.txt files.
-            // IEnumerable<Cost> costs = Directory
-            //     // Select the file names from the directory
-            //     .EnumerateFiles(appDataPath, "*.costs.txt")
-            //     // Each file name is used to create a new Cost
-            //     .Select(fileName => new Cost()
-            //     {
-            //         FileName = fileName,
-            //         Description = File.ReadAllText(fileName),
-            //         Date = File.GetCreationTime(fileName)
-            //     })
-            //     // With the final collection of costs, order them by date
-            //     .OrderBy(cost => cost.Date);
-
-            // Add each cost into the ObservableCollection
-            foreach (Cost cost in costs.OrderBy(cost => cost.Date))
+            foreach (Cost cost in costs.OrderBy(cost => cost.Date)) 
                 Costs.Add(cost);
         }
     }
