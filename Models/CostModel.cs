effb2070f4d068c2a74f4831dab1876e518c015d
Karol <kaajooj@gmail.com>
2023-04-30 03:26:59 +0200

    Implement INotifyPropertyChanged in CostModel

--- Models/CostModel.cs
+++ Models/CostModel.cs
@@ -1,15 +1,37 @@
-﻿namespace FastCost.Models
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FastCost.Models
 {
-    public class CostModel
+    public class CostModel : INotifyPropertyChanged
     {
         public int Id { get; set; }
 
         public decimal Value { get; set; }
 
         public string Description { get; set; }
- 
-        public DateTime Date { get; set; }
 
         public string FormattedDate => Date.ToString("dd.MM");
+
+        private DateTime _date;
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                if (_date != value)
+                {
+                    _date = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
