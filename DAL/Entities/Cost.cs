cb8cc6fb374cdc5cee9810b1c1f317e8b0cd56b9
Karol <kaajooj@gmail.com>
2023-05-01 18:22:01 +0200

    Add CategoryId and Category prop to Cost

--- DAL/Entities/Cost.cs
+++ DAL/Entities/Cost.cs
@@ -13,5 +13,8 @@ namespace FastCost.DAL.Entities
         public string Description { get; set; }
         
         public DateTime Date { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
