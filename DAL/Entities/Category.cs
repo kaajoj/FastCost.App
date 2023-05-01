7f62c3dd487ae9be9ae1f1e8d14e62c9f507a111
Karol <kaajooj@gmail.com>
2023-05-01 16:56:22 +0200

    Add Category entity

---
+++ DAL/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace FastCost.DAL.Entities
+{
+    [Table("categories")]
+    public class Category
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
