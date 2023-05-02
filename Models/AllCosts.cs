3a7317e8f899cb49383715cc19654706bd079541
Karol <kaajooj@gmail.com>
2023-05-03 01:14:28 +0200

    workaround with linking category to cost

--- Models/AllCosts.cs
+++ Models/AllCosts.cs
@@ -17,7 +17,15 @@ namespace FastCost.Models
 
             var results = await App.CostRepository.GetCostsAsync();
             var costs = results.Adapt<List<CostModel>>();
-            
+
+            // workaround with linking category to cost
+            // TODO: db tables relation
+            var categories = await App.CategoryRepository.GetCategoriesAsync();
+            foreach (var cost in costs)
+            {
+                cost.Category = categories.SingleOrDefault(cat => cat.Id == cost.CategoryId);
+            }
+
             foreach (CostModel cost in costs.OrderBy(cost => cost.Date)) 
                 Costs.Add(cost);
         }
