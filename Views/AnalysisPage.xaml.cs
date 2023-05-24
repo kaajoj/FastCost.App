f5eaaa96ecdbea7e1578afab5c4396b2ce4a4a93
Karol <kaajooj@gmail.com>
2023-05-24 19:37:04 +0200

    analysis page init implementation

--- Views/AnalysisPage.xaml.cs
+++ Views/AnalysisPage.xaml.cs
@@ -1,9 +1,34 @@
-﻿namespace FastCost.Views;
+﻿using FastCost.Models;
+
+namespace FastCost.Views;
 
 public partial class AnalysisPage : ContentPage
 {
+    private AllCosts _allCosts;
+
     public AnalysisPage()
     {
         InitializeComponent();
+
+        _allCosts = new AllCosts();
+        BindingContext = _allCosts;
     }
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs state)
+    {
+        // base.OnNavigatedTo(state);
+
+        var currentMonth = DateTime.UtcNow.Date.Month;
+        ((AllCosts)BindingContext)?.LoadCostsByMonth(currentMonth);
+        // ((AllCosts)BindingContext)?.LoadCosts();
+
+        // SumText.Text = $"Sum:  {Task.Run(() => ((AllCosts)BindingContext)?.GetSum(currentMonth).Result.ToString()).Result}";
+    }
+
+    // private async void MyDatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    // {
+    //     DateTime selectedDate = e.NewDate;
+    //     await _allCosts.LoadCostsByMonth(selectedDate.Month);
+    //     SumText.Text = $"Sum:  {Task.Run(() => ((AllCosts)BindingContext)?.GetSum(selectedDate.Month).Result.ToString()).Result}";
+    // }
 }
