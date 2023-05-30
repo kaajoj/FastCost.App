40183eec4a40782b7ed9b25c487c4f0b05a5327e
Karol <kaajooj@gmail.com>
2023-05-30 21:58:52 +0200

    AnalysisPage v2

--- Views/AnalysisPage.xaml.cs
+++ Views/AnalysisPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using FastCost.Models;
+﻿using FastCost.DAL.Entities;
+using FastCost.Models;
 
 namespace FastCost.Views;
 
@@ -6,21 +7,44 @@ public partial class AnalysisPage : ContentPage
 {
     private AllCosts _allCosts;
 
+    public IEnumerable<IGrouping<Category, CostModel>> GroupCosts { get; set; }
+
     public AnalysisPage()
     {
         InitializeComponent();
 
-        _allCosts = new AllCosts();
-        BindingContext = _allCosts;
+        // _allCosts = new AllCosts();
+        // BindingContext = _allCosts;
+        BindingContext = new AllCosts();
+
+        // BindingContext = this;
     }
 
-    protected override void OnNavigatedTo(NavigatedToEventArgs state)
+    protected async override void OnNavigatedTo(NavigatedToEventArgs state)
     {
         // base.OnNavigatedTo(state);
 
         var currentMonth = DateTime.UtcNow.Date.Month;
-        ((AllCosts)BindingContext)?.LoadCostsByMonth(currentMonth);
+        await ((AllCosts)BindingContext)?.LoadCostsByMonth(currentMonth);
         // ((AllCosts)BindingContext)?.LoadCosts();
+        // var costsByCategory = _allCosts.Costs.GroupBy(cost => cost.Category.Name);
+        // IEnumerable<IGrouping<Category, CostModel>>[] costsByCategory = { Task.Run(() => ((AllCosts)BindingContext)?.GetCostsByMonthAndCategory(currentMonth).Result).Result};
+        var costsByCategory = await ((AllCosts)BindingContext)?.GetCostsByMonthGroupByCategory(currentMonth);
+        // GroupCosts = costsByCategory;
+        foreach (var entry in costsByCategory)
+        {
+            // Display the category name.
+            var category = entry.Key;
+            await Console.Out.WriteLineAsync(category.Name);
+            foreach (var cost in entry)
+            {
+                // Display the cost information.
+                Console.WriteLine($"Cost: {cost.Value}, Description: {cost.Description}, Date: {cost.Date}");
+            }
+        }
+        GroupCosts = costsByCategory;
+
+        BindingContext = this;
 
         // SumText.Text = $"Sum:  {Task.Run(() => ((AllCosts)BindingContext)?.GetSum(currentMonth).Result.ToString()).Result}";
     }
