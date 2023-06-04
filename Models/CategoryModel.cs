62c6d13c4a2d7eec02a829f9b1aec9b34f74034e
Karol <kaajooj@gmail.com>
2023-06-04 09:46:25 +0200

    add CategoryModel

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FastCost.Models
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int SumValue { get; set; }
+    }
+}
