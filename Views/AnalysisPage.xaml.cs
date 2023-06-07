319a6f489f933c0faca620ec0712d11b6346d1a9
Karol <kaajooj@gmail.com>
2023-06-07 19:39:40 +0200

    refactor Analysis page

--- Views/AnalysisPage.xaml.cs
+++ Views/AnalysisPage.xaml.cs
@@ -5,14 +5,14 @@ namespace FastCost.Views;
 
 public partial class AnalysisPage : ContentPage
 {
-    public IEnumerable<IGrouping<Category, CostModel>> GroupCosts { get; set; }
-    public IEnumerable<IGrouping<CategoryModel, CostModel>> GroupCosts2 { get; set; }
+    // public IEnumerable<IGrouping<Category, CostModel>> GroupCosts { get; set; }
+    // public IEnumerable<IGrouping<CategoryModel, CostModel>> GroupCosts2 { get; set; }
 
     public AnalysisPage()
     {
         InitializeComponent();
 
-        BindingContext = new AllCosts();
+        // BindingContext = new AllCosts();
 
         // BindingContext = this;
     }
@@ -22,17 +22,17 @@ public partial class AnalysisPage : ContentPage
         // base.OnNavigatedTo(state);
 
         var currentMonth = DateTime.UtcNow.Date.Month;
-        GroupCosts = await ((AllCosts)BindingContext)?.GetCostsByMonthGroupByCategory(currentMonth);
+        await ((AllCosts)BindingContext)?.GetCostsByMonthGroupByCategory(currentMonth);
 
-        foreach (var costGroup in GroupCosts)
-        {
-            decimal? sum = decimal.Zero;
-            foreach (var cost in costGroup)
-            {
-                sum += cost.Value;
-            }
-            costGroup.Key.SumValue = sum;
-        }
+        // foreach (var costGroup in GroupCosts)
+        // {
+        //     decimal? sum = decimal.Zero;
+        //     foreach (var cost in costGroup)
+        //     {
+        //         sum += cost.Value;
+        //     }
+        //     costGroup.Key.SumValue = sum;
+        // }
 
         SumText.Text = $"Total sum:  {Task.Run(() => ((AllCosts)BindingContext)?.GetSum(currentMonth).Result.ToString()).Result}";
 
@@ -42,7 +42,7 @@ public partial class AnalysisPage : ContentPage
     private async void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
     {
         DateTime selectedDate = e.NewDate;
-        GroupCosts = await ((AllCosts)BindingContext)?.GetCostsByMonthGroupByCategory(selectedDate.Month);
+        await ((AllCosts)BindingContext)?.GetCostsByMonthGroupByCategory(selectedDate.Month);
         SumText.Text = $"Total sum:  {Task.Run(() => ((AllCosts)BindingContext)?.GetSum(selectedDate.Month).Result.ToString()).Result}";
 
     }
