2b6a524ee85f4f61bbffe247d001625875208a42
Karol <kaajooj@gmail.com>
2023-06-07 19:41:59 +0200

    clean up all costs page

--- Views/AllCostsPage.xaml.cs
+++ Views/AllCostsPage.xaml.cs
@@ -4,13 +4,9 @@ namespace FastCost.Views;
 
 public partial class AllCostsPage : ContentPage
 {
-    // private AllCosts _allCosts;
-
     public AllCostsPage()
 	{
         InitializeComponent();
-        // _allCosts = new AllCosts();
-        // BindingContext = _allCosts;
     }
 
     protected override async void OnNavigatedTo(NavigatedToEventArgs state)
@@ -19,7 +15,6 @@ public partial class AllCostsPage : ContentPage
 
         var currentMonth = DateTime.UtcNow.Date.Month;
         await ((AllCosts)BindingContext)?.LoadCostsByMonth(currentMonth);
-        // ((AllCosts)BindingContext)?.LoadCosts();
 
         SumText.Text = $"Total sum:  {Task.Run(() => ((AllCosts)BindingContext)?.GetSum(currentMonth).Result.ToString()).Result}";
     }
@@ -36,7 +31,6 @@ public partial class AllCostsPage : ContentPage
             // Get the cost model
             var cost = (CostModel)e.CurrentSelection[0];
 
-            // Should navigate to "CostPage?ItemId=path\on\device\XYZ.costs.txt"
             await Shell.Current.GoToAsync($"{nameof(CostPage)}?{nameof(CostPage.ItemId)}={cost.Id}");
 
             // Unselect the UI
