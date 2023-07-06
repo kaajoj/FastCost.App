e212ca6642ae593d518e736031669c32e499b32d
Karol <kaajooj@gmail.com>
2023-07-07 01:40:11 +0200

    add 'Export costs data' to side menu

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,11 +1,38 @@
-﻿namespace FastCost;
+﻿using FastCost.Models;
+using System.Windows.Input;
+
+namespace FastCost;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
+    public ICommand MyCommand { get; private set; }
+
+    public AppShell()
 	{
 		InitializeComponent();
 
         Routing.RegisterRoute(nameof(Views.CostPage), typeof(Views.CostPage));
+
+        MyCommand = new Command(ExportData);
+        BindingContext = this;
+    }
+
+    private async void ExportData()
+    {
+        var allCosts = new AllCosts();
+        var costs = await allCosts.LoadCostsBackUp();
+
+        var filePath = Path.Combine(FileSystem.AppDataDirectory, "costsBackUp.csv");
+
+        var lines = new List<string>();
+        lines.Add("Id,Value,Description,Date,CategoryId");
+        foreach (var cost in costs)
+        {
+            lines.Add($"{cost.Id},{cost.Value},{cost.Description},{cost.Date},{cost.CategoryId}");
+        }
+
+        File.WriteAllLines(filePath, lines);
+
+        await DisplayAlert("Success", "Costs data exported to file.", "OK");
     }
 }
