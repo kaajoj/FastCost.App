0f6de031b8fa588b7d9c470b3057154d9f5082e2
Karol <kaajooj@gmail.com>
2023-12-30 21:58:21 +0100

    add IAllCostsService

---
+++ Services/IAllCostsService.cs
@@ -0,0 +1,13 @@
+﻿using FastCost.DAL.Entities;
+using FastCost.Models;
+
+namespace FastCost.Services
+{
+    public interface IAllCostsService
+    {
+        public Task<List<Cost>> LoadCostsBackUp();
+        public Task LoadCostsByMonth(int month);
+        public Task<decimal> GetSum(int month);
+        public Task<IEnumerable<IGrouping<Category, CostModel>>> GetCostsByMonthGroupByCategory(int month);
+    }
+}
