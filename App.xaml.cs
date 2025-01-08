b9d26f57c785a91044b44a6cc8a11530fd27d54d
Karol <kaajooj@gmail.com>
2025-01-08 01:30:25 +0100

    overriding Application.CreateWindow

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,21 +9,17 @@ public partial class App : Application
     internal static CategoryRepository CategoryRepository;
     internal static AllCostsService AllCostsService;
 
-    [Obsolete("'Application.MainPage.set' is obsolete: 'This property is deprecated. " +
-        "Initialize your application by overriding Application.CreateWindow rather than setting MainPage." +
-        " To modify the root page in an active application, use Windows[0].Page for applications with" +
-        " a single window. For applications with multiple windows, use Application.Windows to identify and" +
-        " update the root page on the correct window.  Additionally, each element features a Window property," +
-        " accessible when it's part of the current window.'")]
     public App(CostRepository costRepository, CategoryRepository categoryRepository, AllCostsService allCostsService)
 	{
         InitializeComponent();
 
-
-		MainPage = new AppShell();
-
         CostRepository = costRepository;
         CategoryRepository = categoryRepository;
         AllCostsService = allCostsService;
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
     }
 }
