7c465c327c181a3af01eb6d34d315498537ec150
Karol <kaajooj@gmail.com>
2025-01-08 17:43:49 +0100

    remove package reference for Controls.Compatibility

--- App.xaml.cs
+++ App.xaml.cs
@@ -18,7 +18,7 @@ public partial class App : Application
         AllCostsService = allCostsService;
     }
 
-    protected override Window CreateWindow(IActivationState? activationState)
+    protected override Window CreateWindow(IActivationState activationState)
     {
         return new Window(new AppShell());
     }
